a461bc32bf48e646d1c479bdcee7c4fc98db8641
MelvinMMM <melvin.mateta@gmail.com>
2026-03-14 15:21:18 +0100

    Correction de tailwindCSS et ajout du sidebar

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-import * as THREE from 'https://esm.sh/three';
+import * as THREE from 'three';
+import Globe from 'globe.gl';
 
-// URL de la texture des nuages et paramètres de rotation
-const cloudsUrl = './assets/clouds.png';
-const CLOUDS_ALT = 0.004;
-const cloudSpeedRotation = -0.006;
+const earthDiv = document.getElementById('globe');
 
-// Création de la Terre avec les textures et le relief
-const earth = new Globe(document.getElementById('globe'), { animateIn: false })
+// Création du globe
+const earth = Globe()(earthDiv, {animateIn: false})
     .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg')
-    .bumpImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png');
+    .bumpImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png')
+    .width(earthDiv.offsetWidth);
 
 earth.controls().autoRotate = true;
 earth.controls().autoRotateSpeed = 0.35;
 
-// Ajout des nuages
-new THREE.TextureLoader().load(cloudsUrl, cloudsTexture => {
+// Les nuages
+new THREE.TextureLoader().load('./assets/clouds.png', cloudsTexture => {
     const clouds = new THREE.Mesh(
-        new THREE.SphereGeometry(earth.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+        new THREE.SphereGeometry(earth.getGlobeRadius() * (1 + 0.005), 75, 75),
         new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
     );
     earth.scene().add(clouds);
-// Animation de la rotation des nuages
+
     (function rotateClouds() {
-        clouds.rotation.y += cloudSpeedRotation * Math.PI / 180;
+        clouds.rotation.y += -0.005 * Math.PI / 180;
         requestAnimationFrame(rotateClouds);
     })();
 });
 
-// Lorsque l'on clique sur la Terre, la caméra se déplace vers le point cliqué et la rotation automatique s'active  
-earth.onGlobeClick(({ lat, lng }, event) => {
-    earth.pointOfView({ lat, lng, altitude: 0.4 }, 1000);
-});
-
+// Coordonnées au pif pour tester
 const data = [
-    { lat: 48.8566, lng: 2.3522, size: 0.001, color: 'red' }, // Paris
-    { lat: 40.7128, lng: -74.0060, size: 0.001, color: 'blue' }, // New York
+    { lat: 48.8566, lng: 2.3522, size: 0.001, color: 'red', label: 'Paris' },
+    { lat: 40.7128, lng: -74.0060, size: 0.001, color: 'blue', label: 'New York' },
 ];
 
 earth.pointsData(data)
-    .pointColor('color')
-    .pointAltitude('size')
-    .pointRadius(0.5);
+    .pointAltitude(0)    // On enlève de la hauteur (effet bâton/cône)
+    .pointRadius(0.2);   // Diamètre du marqueur
 
-// Lorsque l'on clique sur la souris, la rotation automatique s'arrête, et lorsqu'on relâche le click, elle reprend
+
+
+// Quand on click sur le globe, on se déplace vers le point cliqué
+earth.onGlobeClick(({ lat, lng }) => {
+    earth.pointOfView({ lat, lng, altitude: 0.5 }, 1000);
+});
+
+// Quand on clique et maintient le clic, on arrête la rotation automatique du globe, et quand on relâche, on la redémarre
 window.addEventListener("mousedown", () => {
     earth.controls.autoRotate = false;
 }, false);
@@ -51,8 +52,21 @@ window.addEventListener("mouseup", () => {
     earth.controls.autoRotate = true;
 }, false);
 
-// Lorsque l'on clique sur le bouton "mouvement", la rotation automatique s'arrête ou reprend
-const mouvement = document.getElementById('mouvement');
-mouvement.addEventListener('click', () => {
+
+// quand on click sur le boutton, on arrête ou on démarre la rotation automatique du globe et on change le texte du bouton
+const movement = document.getElementById('movement');
+let textButton = "STOP";
+movement.textContent = textButton;
+movement.addEventListener('click', () => {
     earth.controls().autoRotate = !earth.controls().autoRotate;
+    const Rotating = earth.controls().autoRotate;
+    textButton = Rotating ? "STOP" : "START";
+    movement.textContent = textButton;
+});
+
+// Quand on click sur le boutton, on dézoome sur le globe
+const zoomOut = document.getElementById('zoomOut');
+console.log(zoomOut);
+zoomOut.addEventListener('click', () => {
+    earth.pointOfView({ lat: 0, lng: 0, altitude: 2.5 }, 1000);
 });
