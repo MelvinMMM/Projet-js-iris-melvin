674446c4f07140ef47233f3b577835bb7ddc8622
MelvinMMM <melvin.mateta@gmail.com>
2026-03-15 18:36:53 +0100

    pour être honnête, je ne sais même plus ce que j'ai changé x)

--- main.js
+++ main.js
@@ -29,10 +29,11 @@ new THREE.TextureLoader().load('./assets/clouds.png', cloudsTexture => {
 // Coordonnées au pif pour tester
 const data = [
     { lat: 48.8566, lng: 2.3522, size: 0.001, color: 'red', label: 'Paris' },
-    { lat: 40.7128, lng: -74.0060, size: 0.001, color: 'blue', label: 'New York' },
+    { lat: 40.7128, lng: -74.0060, size: 0.001, color: 'yellow', label: 'New York' },
 ];
 
 earth.pointsData(data)
+    .pointColor('color')  // Couleur du marqueur
     .pointAltitude(0)    // On enlève de la hauteur (effet bâton/cône)
     .pointRadius(0.2);   // Diamètre du marqueur
 
