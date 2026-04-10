09e41d68831e615b73da5f1f05f1ed418d88c306
MelvinMMM <melvin.mateta@gmail.com>
2026-04-10 16:43:09 +0200

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/Projet-js-iris-melvin/',
   plugins: [
     tailwindcss({
       // Disable Lightning CSS optimization
